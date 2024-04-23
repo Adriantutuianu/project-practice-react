71677aa5c3f6747158d7217537c858658f1e12a0
Adrian Tut <adriantutuianu01@gmail.com>
2024-04-23 22:41:35 +0100

    Added refresh button for random number

--- src/compoents/api2-Random-facts/Api2.jsx
+++ src/compoents/api2-Random-facts/Api2.jsx
@@ -6,37 +6,40 @@ const Api2 = () => {
   const [fact, setFact] = useState("");
   const [number, setNumber] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const options = {
-        method: "GET",
-        url: "https://numbersapi.p.rapidapi.com/random/trivia",
-        params: {
-          min: "10",
-          max: "20",
-          fragment: "true",
-          json: "true",
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "331e31b9e6msh309144b3b69a4c8p1e50aajsn05414db71f6f",
-          "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
-        },
-      };
-
-      try {
-        const response = await axios.request(options);
-        if (response.data && response.data.number && response.data.text) {
-          setNumber(response.data.number);
-          setFact(response.data.text);
-        }
-      } catch (error) {
-        console.error(error);
-      }
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      url: "https://numbersapi.p.rapidapi.com/random/trivia",
+      params: {
+        min: "10",
+        max: "20",
+        fragment: "true",
+        json: "true",
+      },
+      headers: {
+        "X-RapidAPI-Key": "331e31b9e6msh309144b3b69a4c8p1e50aajsn05414db71f6f",
+        "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
+      },
     };
 
+    try {
+      const response = await axios.request(options);
+      if (response.data && response.data.number && response.data.text) {
+        setNumber(response.data.number);
+        setFact(response.data.text);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  const handleRefresh = () => {
+    fetchData();
+  };
 
   return (
     <div>
@@ -46,6 +49,7 @@ const Api2 = () => {
           Number: {number} - {fact}
         </p>
       )}
+      <button onClick={handleRefresh}>Refresh</button>
     </div>
   );
 };
