9c12bfaaa72bb83fec5795bf0744ad842595b1c5
Adrian Tut <adriantutuianu01@gmail.com>
2024-04-23 22:13:30 +0100

    Separated Api's

---
+++ src/compoents/api2-Random-facts/Api2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./api2.css";
+
+const Api2 = () => {
+  return <div>Api2</div>;
+};
+
+export default Api2;
--- src/compoents/home/Home.jsx
+++ src/compoents/home/Home.jsx
@@ -1,79 +1,13 @@
-import React, { useState } from "react";
-import axios from "axios";
+import Api1 from "../api1-Math-facts/Api1";
+import Api2 from "../api2-Random-facts/Api2";
+import "./home.css";
 
 const Home = () => {
-  const [number, setNumber] = useState(""); // State to hold the user-entered number
-  const [fact, setFact] = useState(""); // State to hold the fetched fact
-  const [isLoading, setIsLoading] = useState(false); // State to track loading state
-  const [enteredNumber, setEnteredNumber] = useState(""); // State to hold the entered number
-
-  const fetchFact = async () => {
-    if (!number.trim()) {
-      alert("Please enter a valid number.");
-      return;
-    }
-
-    setIsLoading(true); // Set loading state to true
-
-    try {
-      const response = await axios.get(
-        `https://numbersapi.p.rapidapi.com/${number}/math`,
-        {
-          params: {
-            fragment: "true",
-            json: "true",
-          },
-          headers: {
-            "X-RapidAPI-Key":
-              "331e31b9e6msh309144b3b69a4c8p1e50aajsn05414db71f6f",
-            "X-RapidAPI-Host": "numbersapi.p.rapidapi.com",
-          },
-        }
-      );
-
-      setFact(response.data.text); // Update state with the fetched fact
-      setEnteredNumber(number); // Store the entered number
-    } catch (error) {
-      console.error(error);
-      alert("Failed to fetch data. Please try again.");
-    } finally {
-      setIsLoading(false); // Set loading state back to false
-      setNumber(""); // Clear the input field by resetting the number state
-    }
-  };
-
-  const handleInputChange = (event) => {
-    setNumber(event.target.value); // Update the number state as user types
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault(); // Prevent default form submission behavior
-    fetchFact(); // Call fetchFact when the form is submitted
-  };
-
   return (
-    <div>
-      <h1>Math Fact</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="number"
-          value={number}
-          onChange={handleInputChange}
-          placeholder="Enter a number"
-        />
-        <button type="submit">Get Fact</button>
-      </form>
-
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : fact ? (
-        <div>
-          <p>You entered: {enteredNumber}</p>
-          <p>{fact}</p>
-        </div>
-      ) : null}
-    </div>
+    <>
+      <Api1 />
+      <Api2 />
+    </>
   );
 };
-
 export default Home;
